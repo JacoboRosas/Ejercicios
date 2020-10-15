206c1a121e3802e4f4da7a67da56814b94618bd4
Jonathan Jacobo <62814894+JacoboRosas@users.noreply.github.com>
2020-10-15 15:22:17 -0700

    Add files via upload

---
+++ Ejercicio1.c
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h> 
+
+using namespace std;
+
+int main()
+{
+    cout<<"N \ Cuadrado \ Cubo \ Cuarta"<<endl;
+    for(int i=1;i<=20;i++){
+        cout<<i<<"\t";
+        cout<<pow(i,2)<<"\t";
+        cout<<pow(i,3)<<"\t";
+        cout<<pow(i,4)<<"\t";
+        cout<<endl;
+    }
+
+    return 0;
+}
