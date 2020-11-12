771aa1a80c9a43410e4841892d2af6fdc4dbcf05
Jonathan Jacobo <62814894+JacoboRosas@users.noreply.github.com>
2020-11-12 09:39:07 -0800

    Update holaMundo.s

--- holaMundo.s
+++ holaMundo.s
@@ -1,19 +1,16 @@
-extern printf
+@ use_gdb.s
+@ demo program
+.section	.data
+.section	.text
+.globl		_start
+_start:
+mov r1, $5	@ load r1 with 5
+cmp r1, $4	@ compare r1 with 4
+sub r1, r1, $1	@ subtract 1 
+cmp r1, $4      @ r1 now DOES equal 4
+sub r1, r1, $1
+cmp r1, $4
 
-section .text
-
-global main
-
-main:
-    push ebp
-    mov ebp, esp
-
-    push msg
-    call printf
-    add esp, 4
-
-    leave
-    ret
-
-section .data
-    msg     db  'Hello, world!', 13, 10, 0
+mov r7, $1	@ exit syscall
+svc $0		@ wake kernel
+.end
